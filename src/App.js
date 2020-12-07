83e4b016ade78998b9a3367269b2b4b202bf0562
Chinmoy Chakraborty <chinmoychakraborty009@gmail.com>
2020-12-07 22:07:36 +0600

    First commit for e_commerce web app practice client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import {data} from './data';
+
 
 function App() {
+  let productCards = data.products.map(product => {
+    return <div key={product._id} className="card">
+      <img className="medium" src={product.image} alt="product"/>
+      <div className="card-body">
+          <a href={`/product/${product._id}`}>
+              <h2>{product.name}</h2>
+          </a>
+          <div className="rating">
+              <span>
+                  <i className="fa fa-star"></i>
+              </span>
+              <span>
+                  <i className="fa fa-star"></i>
+              </span>
+              <span>
+                  <i className="fa fa-star"></i>
+              </span>
+              <span>
+                  <i className="fa fa-star"></i>
+              </span>
+              <span>
+                  <i className="fa fa-star"></i>
+              </span>
+              <span>
+                  <i className="fa fa-star"></i>
+              </span>
+          </div>
+          <div>
+              <h2>{product.brand}</h2>
+          </div>
+          <div className="price">
+              {product.price}
+          </div>
+      </div> 
+    </div>
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+        <header className="row">
+            <div className="brand">
+                <button onclick="openMenu()">
+                    &#9776;
+                </button>
+                <a href="/">amazona</a>
+            </div>
+            <div className="header-links">
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign In</a>
+            </div>
+        </header>
+        <main className="main">
+            <div className="row center">
+                {productCards}
+            </div>
+        </main>
+        <footer className="row center">
+            All Rights Reserved
+        </footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,69 @@
+export const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Slim Shirt",
+            category: "Shirt",
+            price: "2150 Taka",
+            brand: "Cat's Eye",
+            rating: 4.5,
+            numberOfReviews: 10,
+            description: "High quality product",
+            image: 'images/d1.jpg'
+        },
+        {
+            _id: "2",
+            name: "Slim Shirt",
+            category: "Shirt",
+            price: "1850 Taka",
+            brand: "Yellow",
+            rating: 4,
+            numberOfReviews: 15,
+            description: "High quality product",
+            image: 'images/d2.jpg'
+        },
+        {
+            _id: "3",
+            name: "Slim Shirt",
+            category: "Shirt",
+            price: "1599 Taka",
+            brand: "Plus Point",
+            rating: 3,
+            numberOfReviews: 5,
+            description: "High quality product",
+            image: 'images/d3.jpg'
+        },
+        {
+            _id: "4",
+            name: "Slim Pant",
+            category: "Pant",
+            price: "850 Taka",
+            brand: "Cat's Eye",
+            rating: 5,
+            numberOfReviews: 30,
+            description: "High quality product",
+            image: 'images/d4.jpg'
+        },
+        {
+            _id: "5",
+            name: "Slim Pant",
+            category: "Pant",
+            price: "900 Taka",
+            brand: "Yellow",
+            rating: 4,
+            numberOfReviews: 20,
+            description: "High quality product",
+            image: 'images/d5.jpg'
+        },
+        {
+            _id: "6",
+            name: "Slim Pant",
+            category: "Pant",
+            price: "750 Taka",
+            brand: "Plus Point",
+            numberOfReviews: 4,
+            description: "High quality product",
+            image: 'images/d6.jpg'
+        }
+    ]
+}
