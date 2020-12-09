85cdb1c3c525816000dc8a6f697fb8e5415475b9
Chinmoy Chakraborty <chinmoychakraborty009@gmail.com>
2020-12-10 00:31:08 +0600

    Integrate get products api call to display products in home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,69 @@
-import react from 'react';
+import React from 'react';
 import {data} from '../data';
 import Product from '../components/Product';
+import axios from 'axios';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
 
-function HomeScreen() {
-    let productCards = data.products.map(product => {
-        return <Product key={product._id} product={product}/>
-    });
-    return <div className="row center">
-        {productCards}
-    </div>
+class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products : [],
+            loading : false,
+            error: false
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        try {
+            this.setLoading(true);
+            const {data} = await axios.get('/products');
+            this.setLoading(false);
+            this.setProducts(data);
+        } catch(error) {
+            this.setError(error.message);
+            this.setLoading(false);
+        }
+    }
+
+    setProducts = (data) => {
+        this.setState({
+            products: data.products
+        });
+    }
+
+    setLoading = (loading) => {
+        this.setState({
+            ...this.state,
+            loading: loading
+        })
+    }
+
+    setError = (error) => {
+        this.setState({
+            ...this.state,
+            error : error
+        })
+    }
+
+    render() {
+        let productCards = this.state.products.map(product => {
+            return <Product key={product._id} product={product}/>
+        });
+        return <div>
+            {this.state.loading ? <LoadingBox></LoadingBox> 
+                : this.state.error ? <MessageBox variant="danger">{this.state.error}</MessageBox>
+                : (<div className="row center">
+                    {productCards}
+                </div>
+            )}
+        </div>
+    }
 }
 
 export default HomeScreen;
